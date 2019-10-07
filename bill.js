2d16edcf9d3edde0771d5d7389751c80479162f6
Elune <elune@Siyus-MacBook-Pro.local>
2019-10-07 23:40:23 +1100

    Extract totalAmount calculation and renaming

--- bill.js
+++ bill.js
@@ -5,19 +5,28 @@ function statement (invoice, plays) {
   
     // print line for this order
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
-    totalAmount += amountFor(perf);
+  
   }
+  totalAmount = totalAmount();
 
   result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${totalVolumeCredits()} credits\n`;
   return result;
   
-  function totalVolumeCredits() {
-    let volumeCredits = 0;
+  function totalAmount() {
+    let result = 0;
     for (let perf of invoice.performances) {
-      volumeCredits += volumeCreditsFor(perf);
+      result += amountFor(perf);
     }
-    return volumeCredits;
+    return result;
+  }
+
+  function totalVolumeCredits() {
+    let result = 0;
+    for (let perf of invoice.performances) {
+      result += volumeCreditsFor(perf);
+    }
+    return result;
   }
 
   function usd(aNumber) {
