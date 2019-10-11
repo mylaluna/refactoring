2da4585e9282916e83ab07672a40c6a0cb8cda5f
Rain S. Zhang <rzhang@integradev.com.au>
2019-10-11 16:11:12 +1100

    Add play into Performance Calculator

--- createStatementData.js
+++ createStatementData.js
@@ -17,9 +17,9 @@ export default function createStatementData(invoice, plays) {
   }
 
   function enrichPerformance(aPerformance) {
-    const calculator = new PerformanceCalculator(aPerformance);
+    const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
     const result = Object.assign({}, aPerformance);
-    result.play = playFor(result);
+    result.play = calculator.play;
     result.amount = amountFor(result);
     result.volumeCredits = volumeCreditsFor(result);
     return result;
@@ -60,7 +60,8 @@ export default function createStatementData(invoice, plays) {
 }
 
 class PerformanceCalculator {
-  constructor(aPerformance) {
+  constructor(aPerformance, aPlay) {
     this.performance = aPerformance;
+    this.play = aPlay;
   }
 }
