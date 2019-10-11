474944b53e6d83f31b7e4d1cb3fbf394978a8763
Rain S. Zhang <rzhang@integradev.com.au>
2019-10-11 14:40:49 +1100

    Extract Statement Data creation into a function

--- bill.js
+++ bill.js
@@ -1,10 +1,14 @@
 function statement (invoice, plays) {
+  return renderPlainText(createStatementData(invoice, plays));
+}
+
+function createStatementData(invoice, plays) {
   const statementData = {};
   statementData.customer = invoice.customer;
   statementData.performances = invoice.performances.map(enrichPerformance);
   statementData.totalAmount = totalAmount(statementData);
   statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-  return renderPlainText(statementData, plays);
+  return statementData;
 
   function totalAmount(data) {
     return data.performances
@@ -12,11 +16,8 @@ function statement (invoice, plays) {
   }
 
   function totalVolumeCredits(data) {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.volumeCredits;
-    }
-    return result;
+    return data.performances
+      .reduce((total, p) => total + p.volumeCredits, 0);
   }
 
   function enrichPerformance(aPerformance) {
@@ -61,7 +62,7 @@ function statement (invoice, plays) {
   }
 }
 
-function renderPlainText (data, plays) {
+function renderPlainText (data) {
   let result = `Statement for ${data.customer}\n`;
   for (let perf of data.performances) {
     result += `  ${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`;
