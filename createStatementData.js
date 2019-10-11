1a7d8c77f3b066bb7a0b7529a91cdc4296c18d9f
Rain S. Zhang <rzhang@integradev.com.au>
2019-10-11 16:00:17 +1100

    Create a performance calculartor class

--- createStatementData.js
+++ createStatementData.js
@@ -17,6 +17,7 @@ export default function createStatementData(invoice, plays) {
   }
 
   function enrichPerformance(aPerformance) {
+    const calculator = new PerformanceCalculator(aPerformance);
     const result = Object.assign({}, aPerformance);
     result.play = playFor(result);
     result.amount = amountFor(result);
@@ -55,5 +56,11 @@ export default function createStatementData(invoice, plays) {
 
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
+  }
+}
+
+class PerformanceCalculator {
+  constructor(aPerformance) {
+    this.performance = aPerformance;
   }
 }
